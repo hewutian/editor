ec8f9e9a9f6da250964602052a82ada3376a740f
zy4265856 <agerilibor@gmail.com>
2017-10-18 15:04:14 +0800

    add delete event Hierarchy Listener

--- Assets/Editor/HierarchyListener.cs
+++ Assets/Editor/HierarchyListener.cs
@@ -5,15 +5,110 @@ using UnityEditor;
 using UnityEngine;
 
 [InitializeOnLoad]
-public class EditorMonoBehaviour
+public class HierarchyListener
 {
-    [InitializeOnLoadMethod]
-    static void Start()
+    static HierarchyListener()
     {
-        Selection.selectionChanged = delegate
+        var type = System.Reflection.Assembly.Load("UnityEditor.dll").GetType("UnityEditor.EditorAssemblies");
+        var method = type.GetMethod("SubclassesOf", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(Type) }, null);
+        var e = method.Invoke(null, new object[] { typeof(HierarchyListener) }) as IEnumerable;
+        foreach (Type editorMonoBehaviourClass in e)
         {
-            //Debug.Log(Selection.activeObject.name);
+            method = editorMonoBehaviourClass.BaseType.GetMethod("OnEditorMonoBehaviour", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (method != null)
+            {
+                method.Invoke(System.Activator.CreateInstance(editorMonoBehaviourClass), new object[0]);
+            }
+        }
+    }
+
+    private void OnEditorMonoBehaviour()
+    {
+
+        EditorApplication.update += Update;
+        EditorApplication.hierarchyWindowChanged += OnHierarchyWindowChanged;
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+        EditorApplication.projectWindowChanged += OnProjectWindowChanged;
+        EditorApplication.projectWindowItemOnGUI += ProjectWindowItemOnGUI;
+        EditorApplication.modifierKeysChanged += OnModifierKeysChanged;
+
+
+        // globalEventHandler
+        EditorApplication.CallbackFunction function = () => OnGlobalEventHandler(Event.current);
+        FieldInfo info = typeof(EditorApplication).GetField("globalEventHandler", BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic);
+        EditorApplication.CallbackFunction functions = (EditorApplication.CallbackFunction)info.GetValue(null);
+        functions += function;
+        info.SetValue(null, (object)functions);
+
+
+        EditorApplication.searchChanged += OnSearchChanged;
+
+        EditorApplication.playmodeStateChanged += () => {
+            if (EditorApplication.isPaused)
+            {
+                OnPlaymodeStateChanged(PlayModeState.Paused);
+            }
+            if (EditorApplication.isPlaying)
+            {
+                OnPlaymodeStateChanged(PlayModeState.Playing);
+            }
+            if (EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                OnPlaymodeStateChanged(PlayModeState.PlayingOrWillChangePlaymode);
+            }
         };
 
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void OnHierarchyWindowChanged()
+    {
+
+    }
+
+    public virtual void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+    {
+
+    }
+
+    public virtual void OnProjectWindowChanged()
+    {
+
+    }
+
+    public virtual void ProjectWindowItemOnGUI(string guid, Rect selectionRect)
+    {
+
+    }
+
+    public virtual void OnModifierKeysChanged()
+    {
+
+    }
+
+    public virtual void OnGlobalEventHandler(Event e)
+    {
+
+    }
+
+    public virtual void OnSearchChanged()
+    {
+    }
+
+    public virtual void OnPlaymodeStateChanged(PlayModeState playModeState)
+    {
+
+    }
+
+    public enum PlayModeState
+    {
+        Playing,
+        Paused,
+        Stop,
+        PlayingOrWillChangePlaymode
     }
 }
---
+++ Assets/Editor/HierarchyListenerManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewBehaviourScript : HierarchyListener
+{
+    public static Dictionary<int, GameObject> gameDict = new Dictionary<int, GameObject>();
+    public static List<int> instanceIDArray = new List<int>();
+
+    public override void Update()
+    {
+        //Debug.Log ("每一帧回调一次");
+        instanceIDArray.Clear();
+        GameObject[] pAllObjects = (GameObject[])Resources.FindObjectsOfTypeAll(typeof(GameObject));
+        foreach (GameObject obj in pAllObjects)
+        {
+            if (obj.GetComponent<SceneMark>() != null)
+            { 
+                instanceIDArray.Add(obj.GetInstanceID());
+            }
+        }
+
+        List<int> toDelete = new List<int>();
+        foreach (KeyValuePair<int, GameObject> dic in gameDict)
+        {
+            if (instanceIDArray.Contains(dic.Key))
+                continue;
+            else
+            {
+                toDelete.Add(dic.Key);
+                MapSceneView.Instance.SetMapDesignStage(0);
+            }
+        }
+
+        foreach (int key in toDelete)
+        {
+            gameDict.Remove(key);
+        }
+    }
+
+    public override void OnPlaymodeStateChanged(PlayModeState playModeState)
+    {
+        //Debug.Log ("游戏运行模式发生改变， 点击 运行游戏 或者暂停游戏或者 帧运行游戏 按钮时触发: " + playModeState);
+    }
+
+    public override void OnGlobalEventHandler(Event e)
+    {
+        //Debug.Log ("全局事件回调: " + e);
+    }
+
+    public override void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+    {
+        //Debug.Log (string.Format ("{0} : {1} - {2}", EditorUtility.InstanceIDToObject (instanceID), instanceID, selectionRect));
+        GameObject instance = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+        //Debug.Log(instanceID.ToString());
+        if (instance != null && instance.GetComponent<SceneMark>() != null)
+        {
+            if (!gameDict.ContainsKey(instanceID) || !gameDict.ContainsValue(instance))
+            {
+                gameDict.Add(instanceID, instance);
+            }
+        }
+    }
+
+    public override void OnHierarchyWindowChanged()
+    {
+        //Debug.Log("test");
+    }
+
+    public override void OnModifierKeysChanged()
+    {
+        //	Debug.Log ("当触发键盘事件");
+    }
+
+    public override void OnProjectWindowChanged()
+    {
+        //	Debug.Log ("当资源视图发生变化");
+    }
+
+    public override void ProjectWindowItemOnGUI(string guid, Rect selectionRect)
+    {
+        //根据GUID得到资源的准确路径
+        //Debug.Log (string.Format ("{0} : {1} - {2}", AssetDatabase.GUIDToAssetPath (guid), guid, selectionRect));
+    }
+}
